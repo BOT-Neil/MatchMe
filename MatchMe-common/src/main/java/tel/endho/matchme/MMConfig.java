c52027822a91bc3011453d25a0c2bf793e6c268b
BuildTools <unconfigured@null.spigotmc.org>
2022-03-27 12:34:15 +0200

    savechanges

--- MatchMe-common/src/main/java/tel/endho/matchme/MMConfig.java
+++ MatchMe-common/src/main/java/tel/endho/matchme/MMConfig.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.TreeMap;
 
 public class MMConfig {
+  //todo make a class for groupsettings instead of strings in maps  
+  // TreeMap<GroupClass, TreeMap<ServerStatus, String>>
     public static List<String> motd = new LinkedList<>();
     public static TreeMap<String, TreeMap<ServerStatus, String>> groupMap = new TreeMap<>();
     public static TreeMap<String, Boolean> groupsortOption= new TreeMap<>();
